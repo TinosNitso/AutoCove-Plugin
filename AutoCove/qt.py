e17b51f9f17442b7cbea919326a15c0fef275e68
Konstantinos Nitsopoulos <NITSOPOULOS@MAIL.COM>
2021-12-02 02:57:23 +1300

    Add files via upload

--- AutoCove/qt.py
+++ AutoCove/qt.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon, QMovie
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QPlainTextEdit, QPushButton, QCheckBox, QComboBox, QMessageBox, QFileDialog
-#from electroncash.i18n import _ #Language translator doesn't work on more than one word at a time, at least not when I checked.
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QLabel, QPlainTextEdit, QComboBox
+from electroncash.i18n import _ #Language translator didn't work on more than one word at a time, when I checked.
 from electroncash.plugins import BasePlugin, hook
 import electroncash, threading, zipfile, shutil, time
 from electroncash import bitcoin
@@ -55,53 +55,112 @@ class Plugin(BasePlugin):
             Tabs=self.windows[wallet_name].tabs
             Tabs.setTabIcon(Tabs.indexOf(self.tabs[wallet_name]),self.Icon) #It's probably more elegant to keep track of each tab index using a pyqt5 signal connection, instead of constantly asking for it. I'm not sure how.
 class UI(QDialog):
+    Scripts='''
+210279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f817986fad7b828c7f757ca87bbb7d0144817f770120817f7578aa887882013481947f77587f758178827794025402819458807c012a817f77517f7c817f77517f7c817f75a97c041976a9147e7c7e0288ac7eaa7c82012881947f770120817f758708060000000044434675
+210279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f817986fad7b828c7f757ca87bbb7d0144817f770120817f7578aa887882013481947f77587f758178827794027902819458807c012a817f77517f7c817f77517f7c817f75a97c041976a9147e7c7e0288ac7eaa7882012881947f770120817f7588547f0120817f0120817f770124817f75aa88080600000001292a8675
+'''.splitlines()[1:]
+    Addresses=[electroncash.address.Address.from_multisig_script(bitcoin.bfh(Script)) for Script in Scripts]
+    Assembly=[
+'''# 'preturn...' v1.0.0 Script source-code. I like writing the starting stack items relevant to each line, to the right of it.
+210279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798	#[TXParent, Preimage, Sig] before [, PubKey].
+3DUP CHECKSIGVERIFY  ROT SIZE 1SUB SPLIT DROP  SWAP SHA256  ROT CHECKDATASIGVERIFY	#[..., Preimage, Sig, PubKey] Proof DATA = Preimage
+TUCK 0144 BIN2NUM SPLIT NIP 0120 BIN2NUM SPLIT DROP	#[TX, Preimage] Preimage Outpoint TXID
+OVER HASH256 EQUALVERIFY	#[..., TX, TXID] Proof TXParentID = Outpoint TXID
+OVER SIZE 0134 BIN2NUM SUB SPLIT NIP  8 SPLIT DROP  BIN2NUM	#[Preimage, TX] Calulate input value from Preimage
+OVER SIZE NIP SUB  025402 BIN2NUM SUB  8 NUM2BIN	#[..., TX, Amount] Subtract fee of (SIZE(TXParent)+596).
+SWAP 012a BIN2NUM SPLIT NIP  1 SPLIT  SWAP BIN2NUM SPLIT NIP	# [..., TX, Amount] NIP start & sender sig off TX.
+1 SPLIT  SWAP BIN2NUM SPLIT DROP   HASH160	#[..., TXSPLIT] 1ST input to parent has this address.
+SWAP 041976a914 CAT SWAP CAT 0288ac CAT  HASH256	#[..., Amount, Address] Predict hashOutputs.
+SWAP SIZE 0128 BIN2NUM SUB SPLIT NIP 0120 BIN2NUM SPLIT DROP  EQUAL	#[Preimage,hashOutputs] Proof hashOutputs EQUAL Amount & Address from Parent.
+080600000000444346 DROP #[Bool] Append nonce for vanity address, generated from VanityTXID-Plugin.\n''', 
+
+'''# 'preturn...' v1.0.1 Script source-code. I like writing the starting stack items relevant to each line, to the right of it. This update adds 1 line, a 5% fee increase, to guarantee return has only 1 input.
+210279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798	#[TXParent, Preimage, Sig] before [, PubKey].
+3DUP CHECKSIGVERIFY  ROT SIZE 1SUB SPLIT DROP  SWAP SHA256  ROT CHECKDATASIGVERIFY	#[..., Preimage, Sig, PubKey] Proof DATA = Preimage
+TUCK 0144 BIN2NUM SPLIT NIP 0120 BIN2NUM SPLIT DROP	#[TX, Preimage] Preimage Outpoint TXID
+OVER HASH256 EQUALVERIFY	#[..., TX, TXID] Proof TXParentID = Outpoint TXID
+OVER SIZE 0134 BIN2NUM SUB SPLIT NIP  8 SPLIT DROP  BIN2NUM	#[Preimage, TX] Calulate input value from Preimage
+OVER SIZE NIP SUB  027902 BIN2NUM SUB  8 NUM2BIN	#[..., TX, Amount] Subtract fee of (SIZE(TXParent)+596+37).
+SWAP 012a BIN2NUM SPLIT NIP  1 SPLIT  SWAP BIN2NUM SPLIT NIP	# [..., TX, Amount] NIP start & sender sig off TX.
+1 SPLIT  SWAP BIN2NUM SPLIT DROP   HASH160	#[..., TXSPLIT] 1ST input to parent has this address.
+SWAP 041976a914 CAT SWAP CAT 0288ac CAT  HASH256	#[..., Amount, Address] Predict hashOutputs.
+OVER SIZE 0128 BIN2NUM SUB SPLIT NIP 0120 BIN2NUM SPLIT DROP  EQUALVERIFY	#[Preimage,hashOutputs] Proof hashOutputs EQUAL Amount & Address from Parent.
+4 SPLIT  0120 BIN2NUM SPLIT  0120 BIN2NUM SPLIT NIP  0124 BIN2NUM SPLIT DROP  HASH256 EQUALVERIFY #[Preimage] Proof of only 1 input in return TX.
+080600000001292a86 DROP #[nVersion] Append nonce for vanity address, generated from VanityTXID-Plugin.\n''']
     def __init__(self, window, plugin):
         QDialog.__init__(self, window)
         self.window, self.plugin = window, plugin
         
-        self.HiddenBox=QPlainTextEdit()   #Hidden textbox allow primary Qt thread to broadcast_transaction. Only primary thread is allowed to do this.
-        self.HiddenBox.textChanged.connect(self.broadcast_transaction)
-        self.Active=True
-        threading.Thread(target=self.Thread).start()  
+        self.UTXOs=None    #List of UTXOs to skip over.
+        window.history_updated_signal.connect(self.history_updated)
+        
+        Title=QLabel('AutoCove IDE')
+        Title.setStyleSheet('font-weight: bold'), Title.setAlignment(Qt.AlignCenter)
+        
+        self.ComboBox = QComboBox()
+        self.ComboBox.addItems(['v1.0.0', 'v1.0.1'])
+        self.ComboBox.setCurrentIndex(1)
+        self.ComboBox.activated.connect(self.VersionSwitch)
+        
+        HBox=QHBoxLayout()
+        HBox.addWidget(Title,1)
+        HBox.addWidget(self.ComboBox,.1)
+        
+        self.AssemblyBox=QPlainTextEdit()
+        self.AssemblyBox.setLineWrapMode(QPlainTextEdit.NoWrap)
+        self.AssemblyBox.textChanged.connect(self.Compile)
+
+        self.ScriptBox=QPlainTextEdit()
+        self.ScriptBox.setReadOnly(True)
+        self.VersionSwitch()    #Assembly Script dumped onto ScriptBox.
+
+        LabelBig=QLabel("If a 'preturn...' address (v1.0.1 or v1.0.0) is added to a watching-only wallet, this plugin will automatically enforce the return txns.\nSending txn SIZE must be smaller than 520 Bytes (3 inputs max).\n14 bits minimum for single input, but greater minimum for more inputs.\n8 bits minimum fee.\n21 BCH max, but only a single BCH has ever been tested.\nThe private key is always 1.")
+        {Label.setTextInteractionFlags(Qt.TextSelectableByMouse) for Label in {Title, LabelBig}}
         
         VBox=QVBoxLayout()
-        Label=QLabel("Hi,\nThis is a minimum viable release for the AutoCove-Plugin v1.0.0.\nThe idea is that there will be many covenants who automatically forward payments, depending on their assembly code.\nFor example, address 'ppythagoras...' may only return three coins at a time, and only if the same address sends them, and a²+b²=c² (using OP_DIV we could check a/(c+b)=(c-b)/a).\nHere we'll have the power to add/choose from many covenants, and decode/encode their assembly code etc.\nI don't like Spedn & CashScript, so I might not include them as compilers.\nThe TabIcon will change.\n\nIn the case of 'preturn...', it will return whatever coins are sent to it, automatically, assuming a few conditions.\nSender must use a P2PKH address.\nSending transaction must be no more than 520B. Only 3 inputs at most.\n14 bits minimum for only one input.\n21 BCH max, but I've only tested ~$38 at most with this exact address.\n8 bits minimum fee.\nbitcoincash:preturnf8g0qd9pte0u4qkkvlk6t42zz2s5a9qj3r4\nsimpleledger:preturnf8g0qd9pte0u4qkkvlk6t42zz2scxwm83at\n\nThe private key is just 1, but that's OK.\n-Tinos")
-        Label.setTextInteractionFlags(Qt.TextSelectableByMouse)
-        VBox.addWidget(Label)
+        VBox.addLayout(HBox)
+        VBox.addWidget(self.AssemblyBox,3)  #3x bigger Assembly.
+        VBox.addWidget(self.ScriptBox,1)
+        VBox.addWidget(LabelBig)
         self.setLayout(VBox)
-    def broadcast_transaction(self): self.window.broadcast_transaction(electroncash.Transaction(self.HiddenBox.toPlainText()),None)
-    def Thread(self):
-        Address=electroncash.address.Address.from_string('preturnf8g0qd9pte0u4qkkvlk6t42zz2s5a9qj3r4')
-        Script='210279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f817986fad7b828c7f757ca87bbb7d0144817f770120817f7578aa887882013481947f77587f758178827794025402819458807c012a817f77517f7c817f77517f7c817f75a97c041976a9147e7c7e0288ac7eaa7c82012881947f770120817f758708060000000044434675'
-        TXRaw='01000000018fa0a26cc66f0ce4d7945f7618fbdbb352c80ec52b34c6a0cd7c730265edef0500000000fdba024cbe0100000001b744c5d4ae66053c8a7a6a5147ac1bb69d4e4b1458fd46bbb55acf31c8000000000000006b483045022100affc94adc0821c512164dab640f9537caa8e2c900df10093c900f74c480e78a8022070ca7ce866b76d0025e836f6f807a6e8e7ffff2db64d4a90ce9933a7b945044a4121025fbed04ee6f174942ccbadbaf8a5044728ee5bc52dda3d1b1698c173927b69b2feffffff01568100000000000017a914f2be0e693a1e06942bcbf9505accfdb4baa842548718ed0a004d280101000000bb31e6a63c2f02a0c2ef75f7f663f405c4f85a4f3fb52e43ee1cd0b764d478ca18606b350cd8bf565266bc352f0caddcf01e8fa789dd8a15386327cf8cabe1988fa0a26cc66f0ce4d7945f7618fbdbb352c80ec52b34c6a0cd7c730265edef05000000008b210279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f817986fad7b828c7f757ca87bbb7d0144817f770120817f7578aa887882013481947f77587f758178827794025402819458807c012a817f77517f7c817f77517f7c817f75a97c041976a9147e7c7e0288ac7eaa7c82012881947f770120817f7587080600000000444346755681000000000000feffffff8b84c80afed89b07268d8f289a6c83f2898b17eb52e31e8b19b2087daa679d2d18ed0a004100000041cb288e13d73f2807afe6a01aae86b734fd4b311ee19a03061c34d3b5d58cc51e773ec4220c441acbaf2a1fcefcfb96ab9f6caf225d42555fba569d935fcb3637414c8b210279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f817986fad7b828c7f757ca87bbb7d0144817f770120817f7578aa887882013481947f77587f758178827794025402819458807c012a817f77517f7c817f77517f7c817f75a97c041976a9147e7c7e0288ac7eaa7c82012881947f770120817f758708060000000044434675feffffff01447e0000000000001976a91400000015f446176542da094175afedd60c891f3888ac18ed0a00'
-        wallet=self.window.wallet   #The TXRaw is a starting example which is edited as new txns occur. We check every second.
-        UTXOskip={None}  #Keep track of UTXOs to skip over.
-        while self.Active:
-            for UTXO in wallet.get_utxos():
-                if UTXO['prevout_hash'] in UTXOskip: continue
-                UTXOskip.add(UTXO['prevout_hash'])
-                
-                if Address!=UTXO['address']: continue
-                UTX=electroncash.Transaction(wallet.storage.get('transactions')[UTXO['prevout_hash']])
-                
-                SInput = UTX.inputs()[0]    #Spent Input. The sender demands their money returned.
-                if SInput['type']!='p2pkh': continue
-                
-                Amount = UTXO['value']-(596+UTX.estimated_size())
-                if Amount<546: continue #Dust
-                
-                TXRaw=TXRaw[:-8]+UTX.raw[-8:] #Copy the UTXO time.
-                TX=electroncash.Transaction(TXRaw)
-                TX.outputs()[0]=(0,SInput['address'],Amount)    #I've checked the Covenant Script will fail if address or Amount are any different.
-                
-                Input=TX.inputs()[0]
-                Input['prevout_n'] = UTXO['prevout_n']  #Covenant's relative index.
-                Input['prevout_hash'] = UTXO['prevout_hash']
-                Input['value']=UTXO['value']
-                Input['scriptCode']=Script
-                PreImage=TX.serialize_preimage(0)
-                Sig=electroncash.schnorr.sign((1).to_bytes(32,'big'),bitcoin.Hash(bitcoin.bfh(PreImage)))
-                Input['scriptSig']=bitcoin.push_script(UTX.raw)+bitcoin.push_script(PreImage)+bitcoin.push_script(Sig.hex()+'41')+bitcoin.push_script(Script)
-                TXRaw=TX.serialize()
-                self.HiddenBox.setPlainText(TXRaw)
-            time.sleep(1)
+    def history_updated(self):
+        window=self.window
+        wallet=window.wallet   
+        UTXOs=wallet.get_utxos()
+        if self.UTXOs==UTXOs: return    #No change in UTXOs.
+        self.UTXOs=UTXOs
+        for UTXO in UTXOs:
+            try: index=self.Addresses.index(UTXO['address'])
+            except: continue    #Not an AutoCove UTXO.
+            UTX=electroncash.Transaction(wallet.storage.get('transactions')[UTXO['prevout_hash']])
+            
+            SInput = UTX.inputs()[0]    #Spent Input. The sender demands their money returned. Covenant requires input 0 is sender.
+            if SInput['type']!='p2pkh': continue
+            
+            Amount = UTXO['value']-(596+37*index+UTX.estimated_size())  #v1.0.1 costs precisely 37 sats extra, to block multi-input return. (Technically 36 Bytes, but I set the covenant to 37 sats.)
+            if Amount<546: continue #Dust
+            
+            TX=UTX  #Copy nLocktime.
+            TX.inputs().clear(), TX.outputs().clear()
+            TX.outputs().append((0,SInput['address'],Amount))    #I've verified the Covenant Script will fail if address or Amount are any different.
+            
+            UTXO['type'], UTXO['scriptCode'] = 'unknown', self.Scripts[index]
+            TX.inputs().append(UTXO)    #Covenant also requires return TX have only 1 input.
+
+            PreImage=TX.serialize_preimage(0)
+            Sig=electroncash.schnorr.sign((1).to_bytes(32,'big'),bitcoin.Hash(bitcoin.bfh(PreImage)))
+            TX.inputs()[0]['scriptSig']=bitcoin.push_script(UTX.raw)+bitcoin.push_script(PreImage)+bitcoin.push_script(Sig.hex()+'41')+bitcoin.push_script(self.Scripts[index])
+            window.broadcast_transaction(electroncash.Transaction(TX.serialize()),None) #Throws error if we don't call .Transaction.
+    def Compile(self):
+        Assembly=''.join(Line.split('#')[0].upper()+' ' for Line in self.AssemblyBox.toPlainText().splitlines())    #This removes all line breaks & comments from assembly code.
+        Script=''
+        for Str in Assembly.split():
+            try:        Script+=bitcoin.int_to_hex(eval('electroncash.address.OpCodes.OP_'+Str).value)
+            except: 
+                try:    Script+=bitcoin.int_to_hex(eval('electroncash.address.OpCodes.'+Str).value)
+                except: Script+=Str
+        try:    Address=electroncash.address.Address.from_multisig_script(bitcoin.bfh(Script)).to_ui_string()
+        except: Address=''
+        self.ScriptBox.setPlainText(Script.lower()+'\n'+str(int(len(Script)/2))+' Bytes scriptCode ↑ with BCH address ↓\n'+Address)
+    def VersionSwitch(self): self.AssemblyBox.setPlainText(self.Assembly[self.ComboBox.currentIndex()])    
+        
